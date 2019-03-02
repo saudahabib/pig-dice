bc068d1103a16e7e9a0f0a39065ec0e727785807
saudahabib <saudababs00@gmail.com>
2019-03-02 13:39:16 +0300

    add functionality to detect 1 in array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,32 @@
-// try random generation
-function roll() {
-  document.getElementById('die1').innerHTML= parseInt(Math.random()*6+1);
+var total=0
+var values=[];
+
+function roll1() {
+  values.push(document.getElementById('die1').innerHTML= parseInt(Math.random()*6+1));
+  alert(values)
+}
+
+$(document).ready(function(){
+  $("#rol").click(function(){
+    var nax=0;
+    for(z=0;z<values.length;z++){
+      if(values[z]!=1){
+         nax=nax+values[z];
+      }
+      else {
+        values.length=0;
+      }
+
+    }
+    alert(nax)
+  })
+
+})
+
+
+//calculate total score here
+
+
+function roll2() {
+  document.getElementById('die2').innerHTML= parseInt(Math.random()*6+1);
 }
